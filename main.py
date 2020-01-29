392f2ee1e9b043e118ebc58d80b9e08036791cb3
jakeregele <38086861+jakeregele@users.noreply.github.com>
2020-01-29 09:01:06 -0500

    Created main file

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import sqlite3
 
 def main():
+    user_input = ''
+    while user_input.lower() != 'q':
+        user_input = input("")
+        user_input.split()
+        query_string = parse(user_input)
+        query(query_string)
 
 
-def loadData(fileName):
+
+
+
+def load_data(fileName):
     #one of the commands should be load data, which will create the database and the schema and read
     #data from your csv files into the tables; if the database already exists, then the command will overwrite
     #the existing database
 
 def query(sqlString):
 
-def parse(userString):
+def parse(userList):
+    query_string = ''
 
+    return query_string
