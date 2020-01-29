98e1f536b19bf4baffc86f0d78d9f4f80297b60e
jakeregele <38086861+jakeregele@users.noreply.github.com>
2020-01-29 08:41:22 -0500

    Created main file

---
+++ main.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+def main():
+
+
+def loadData(fileName):
+    #one of the commands should be load data, which will create the database and the schema and read
+    #data from your csv files into the tables; if the database already exists, then the command will overwrite
+    #the existing database
+
+def query(sqlString):
+
+def parse(userString):
+
