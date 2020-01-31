f05bf5f7276012cb233e9ecdc007884986376ace
gdosulli <45126172+gdosulli@users.noreply.github.com>
2020-01-31 08:56:40 -0500

    Added table creation method, outlined load_data

--- main.py
+++ main.py
@@ -19,11 +19,24 @@ def main():
 
 # load data from file
 def load_data(fileName):
+
+    # initialize db tables
+    
+    # populate each with csvs
+    # create RDB connection
+    # overwrite current DB tables in memory
+
     return 0
     # one of the commands should be load data, which will create the database and the schema and read
     # data from your csv files into the tables; if the database already exists, then the command will overwrite
     # the existing database
 
+
+# create db table
+def create_table(db, table):
+    db.cursor()
+    db.execute(table)
+    db.commit()
 
 # query database, load data if not already loaded
 def query(sqlString):
