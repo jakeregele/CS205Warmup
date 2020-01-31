4d342411bf85ae9b5b20f91a309413e088c44005
jakeregele <38086861+jakeregele@users.noreply.github.com>
2020-01-31 09:08:21 -0500

    updates, docs, main loop improvement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
 
 
+# TABLE NAME: top50songs ATTRIBUTE NAMES: Track.Name, Length., Ranking, Artist.Name
+# TABLE NAME: artists ATTRIBUTE NAMES: artist, genre, top ranked song
 def main():
     user_input = ''
 
@@ -11,14 +13,19 @@ def main():
         user_input = input("")
 
         # check to make sure user has not quit
-        if user_input.lower() not in ['q', 'quit']:
+        if user_input.lower() == 'help':
+            print("help")
+
+        elif user_input.lower() not in ['q', 'quit']:
             user_input.split()
             query_string = parse(user_input)
             query(query_string)
 
+    return 0
+
 
 # load data from file
-def load_data(fileName):
+def load_data(file_name):
     return 0
     # one of the commands should be load data, which will create the database and the schema and read
     # data from your csv files into the tables; if the database already exists, then the command will overwrite
@@ -26,12 +33,15 @@ def load_data(fileName):
 
 
 # query database, load data if not already loaded
-def query(sqlString):
+def query(sql_string):
     return 0
 
 
 # translate commands from user into SQL query
-def parse(userList):
+def parse(user_list):
     query_string = ''
+    i = len(user_list)
 
     return query_string
+
+main()
