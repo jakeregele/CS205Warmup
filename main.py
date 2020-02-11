17e90ad822ffac1250d2089654d54c245a714c35
Ethan Bokelberg <ebokelbe@uvm.edu>
2020-02-11 12:48:39 -0500

    fixed an issue with spacing errors

--- main.py
+++ main.py
@@ -12,49 +12,49 @@ def main():
     drop_tables(db, "DROP TABLE top_artists;")
 
     # create db tables
-    create_table(db, "CREATE TABLE top_songs (id INTEGER PRIMARY KEY, song_name CHAR[64], song_length INTEGER, ranking INT, artist INT);")
-    create_table(db, "CREATE TABLE top_artists (id INTEGER PRIMARY KEY, artist_name CHAR[32], genre CHAR[32], top_ranked_song INT);")
+    create_table(db, "CREATE TABLE top_songs(id INTEGER PRIMARY KEY, song_name CHAR[64], song_length INTEGER, ranking INT, artist INT);")
+    create_table(db, "CREATE TABLE top_artists(id INTEGER PRIMARY KEY, artist_name CHAR[32], genre CHAR[32], top_ranked_song INT);")
 
-   # load_data(db)
+    load_data(db)
 
-    # # main program loop for user input
-    # while user_input.lower() not in ['q', 'quit']:
-    #
-    #     # get input from command line
-    #     user_input = input("")
-    #
-    #     # check to make sure user has not quit
-    #     if user_input.lower() not in ['q', 'quit']:
-    #         user_input.split()
-    #         query_string = parse(user_input)
-    #         query(query_string)
-    #
+    # main program loop for user input
+    while user_input.lower() not in ['q', 'quit']:
+
+        # get input from command line
+        user_input = input("")
+
+        # check to make sure user has not quit
+        if user_input.lower() not in ['q', 'quit']:
+            user_input.split()
+            query_string = parse(user_input)
+            query(query_string)
+
 
 # load data from file
-# def load_data(db):
-#
-#     # initialize db tables
-#     # populate each with csvs
-#     # load each csv into vector and populate tables with them
-#     top_songs = []
-#     top_artists = []
-#     for row in csv.reader("top50songs.csv"):
-#         top_songs.append(row)
-#
-#     for row in csv.reader("artists.csv"):
-#         top_artists.append(row)
-#
-#     for x in range(1, len(top_songs) + 1):
-#         insert_statement = "INSERT INTO top_songs VALUES " + (x, top_songs[x][0], top_songs[x - 1][1], top_songs[x - 1][2], top_songs[x - 1][3]) + ";"
-#         db.execute(insert_statement)
-#
-#     for x in range(1, len(top_artists) + 1):
-#         insert_statement = "INSERT INTO top_artists VALUES " + (x, top_songs[x][0], top_artists[x - 1][1], top_artists[x - 1][2], top_artists[x - 1][3]) + ";"
-#         db.execute(insert_statement)
-#
-#     db.commit()
-#
-#     return 0
+def load_data(db):
+
+    # initialize db tables
+    # populate each with csvs
+    # load each csv into vector and populate tables with them
+    top_songs = []
+    top_artists = []
+    for row in csv.reader("top50songs.csv"):
+        top_songs.append(row)
+
+    for row in csv.reader("artists.csv"):
+        top_artists.append(row)
+
+    for x in range(1, len(top_songs) + 1):
+        insert_statement = "INSERT INTO top_songs VALUES " + (x, top_songs[x][0], top_songs[x - 1][1], top_songs[x - 1][2], top_songs[x - 1][3]) + ";"
+        db.execute(insert_statement)
+
+    for x in range(1, len(top_artists) + 1):
+        insert_statement = "INSERT INTO top_artists VALUES " + (x, top_songs[x][0], top_artists[x - 1][1], top_artists[x - 1][2], top_artists[x - 1][3]) + ";"
+        db.execute(insert_statement)
+
+    db.commit()
+
+    return 0
     # one of the commands should be load data, which will create the database and the schema and read
     # data from your csv files into the tables; if the database already exists, then the command will overwrite
     # the existing database
@@ -72,6 +72,7 @@ def drop_tables(db, table):
     db.cursor()
     db.execute(table)
     db.commit()
+
 
 # query database, load data if not already loaded
 def query(sqlString):
