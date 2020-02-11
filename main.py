c6bb1f7acdbfea0a7a1babcfced425b64fbea9bc
gdosulli <45126172+gdosulli@users.noreply.github.com>
2020-02-11 12:48:35 -0500

    updated database creation methods, included correct insertion syntax for database

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     user_input = ''
 
     # initialize connection with database
-    db = sqlite3.connect('spotify_data')
+    db = sqlite3.connect('spotify_data.db')
     # create db tables
     create_table(db, "Create Table 1 String Here")
     create_table(db, "Create Table 2 String Here")
@@ -34,18 +34,22 @@ def load_data(db):
     # load each csv into vector and populate tables with them
     top_songs = []
     top_artists = []
-    for row in csv.reader("top50songs.csv"):
-        top_songs.append(row)
 
-    for row in csv.reader("artists.csv"):
-        top_artists.append(row)
+    with open('top50songs.csv') as csvDataFile:
+        for row in csv.reader(csvDataFile):
+            top_songs.append([row[1], row[2], row[3], row[4]])
+
+    with open('artists.csv') as csvDataFile:
+        for row in csv.reader(csvDataFile):
+            top_artists.append([row[1], row[2], row[3]])
+
 
     for x in range(1, len(top_songs) + 1):
-        insert_statement = "INSERT INTO top_songs VALUES " + (x, top_songs[x][0], top_songs[x - 1][1], top_songs[x - 1][2], top_songs[x - 1][3]) + ";"
+        insert_statement = "INSERT INTO top_songs VALUES (" + x + "," + top_songs[x][0] + "," + top_songs[x][1] + "," + top_songs[x][2] + "," + top_songs[x][3] + ");"
         db.execute(insert_statement)
 
     for x in range(1, len(top_artists) + 1):
-        insert_statement = "INSERT INTO top_artists VALUES " + (x, top_songs[x][0], top_artists[x - 1][1], top_artists[x - 1][2], top_artists[x - 1][3]) + ";"
+        insert_statement = "INSERT INTO top_songs VALUES (" + x + "," + top_artists[x][0] + "," + top_artists[x][1] + "," + top_artists[x][2] + ");"
         db.execute(insert_statement)
 
     db.commit()
@@ -72,3 +76,5 @@ def parse(userList):
     query_string = ''
 
     return query_string
+
+main()
