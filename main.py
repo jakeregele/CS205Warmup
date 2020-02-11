acd137291a72b8409377cd211b2c579a8f1610ef
Ethan Bokelberg <ebokelbe@uvm.edu>
2020-02-11 14:01:39 -0500

    overwrite these changes

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ def main():
     drop_tables(db, "DROP TABLE top_songs;")
     drop_tables(db, "DROP TABLE top_artists;")
 
-    db = sqlite3.connect('spotify_data.db')
     # create db tables
-    create_table(db, "CREATE TABLE top_songs(id INTEGER PRIMARY KEY, song_name CHAR[64], song_length INTEGER, ranking INT, artist INT);")
-    create_table(db, "CREATE TABLE top_artists(id INTEGER PRIMARY KEY, artist_name CHAR[32], genre CHAR[32], top_ranked_song INT);")
+    create_table(db, "CREATE TABLE top_songs(id INTEGER PRIMARY KEY, song_name CHAR[64], song_length INTEGER, ranking INT, artist CHAR[64]);")
+    create_table(db, "CREATE TABLE top_artists(id INTEGER PRIMARY KEY, artist_name CHAR[32], genre CHAR[32], top_ranked_song CHAR[64]);")
 
     load_data(db)
 
@@ -44,17 +43,17 @@ def load_data(db):
         for row in csv.reader(csvDataFile):
             top_songs.append([row[1], row[2], row[3], row[4]])
 
-    with open('artists.csv') as csvDataFile:
+    with open('artists.csv', encoding="utf8") as csvDataFile:
         for row in csv.reader(csvDataFile):
             top_artists.append([row[1], row[2], row[3]])
 
 
     for x in range(1, len(top_songs) + 1):
-        insert_statement = "INSERT INTO top_songs VALUES (" + x + "," + top_songs[x][0] + "," + top_songs[x][1] + "," + top_songs[x][2] + "," + top_songs[x][3] + ");"
+        insert_statement = "INSERT INTO top_songs VALUES (" + str(x) + "," + top_songs[x][0] + "," + top_songs[x][1] + "," + top_songs[x][2] + "," + top_songs[x][3] + ");"
         db.execute(insert_statement)
 
     for x in range(1, len(top_artists) + 1):
-        insert_statement = "INSERT INTO top_artists VALUES (" + x + "," + top_artists[x][0] + "," + top_artists[x][1] + "," + top_artists[x][2] + ");"
+        insert_statement = "INSERT INTO top_artists VALUES (" + str(x) + "," + top_artists[x][0] + "," + top_artists[x][1] + "," + top_artists[x][2] + ");"
         db.execute(insert_statement)
 
     db.commit()
