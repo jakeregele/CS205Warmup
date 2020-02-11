50724e7d265c5f0e1cf25ce9e5eb0b0b3a68ba38
gdosulli <45126172+gdosulli@users.noreply.github.com>
2020-02-11 13:27:37 -0500

    Fixed unicode issue with artist csv file

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ def main():
     drop_tables(db, "DROP TABLE top_songs;")
     drop_tables(db, "DROP TABLE top_artists;")
 
-    db = sqlite3.connect('spotify_data.db')
     # create db tables
     create_table(db, "CREATE TABLE top_songs(id INTEGER PRIMARY KEY, song_name CHAR[64], song_length INTEGER, ranking INT, artist INT);")
     create_table(db, "CREATE TABLE top_artists(id INTEGER PRIMARY KEY, artist_name CHAR[32], genre CHAR[32], top_ranked_song INT);")
@@ -40,21 +39,21 @@ def load_data(db):
     top_songs = []
     top_artists = []
 
-    with open('top50songs.csv') as csvDataFile:
-        for row in csv.reader(csvDataFile):
+    with open('top50songs.csv') as csv_songs:
+        for row in csv.reader(csv_songs):
             top_songs.append([row[1], row[2], row[3], row[4]])
 
-    with open('artists.csv') as csvDataFile:
-        for row in csv.reader(csvDataFile):
+    with open('artists.csv') as csv_artists:
+        for row in csv.reader(csv_artists):
             top_artists.append([row[1], row[2], row[3]])
 
 
     for x in range(1, len(top_songs) + 1):
-        insert_statement = "INSERT INTO top_songs VALUES (" + x + "," + top_songs[x][0] + "," + top_songs[x][1] + "," + top_songs[x][2] + "," + top_songs[x][3] + ");"
+        insert_statement = "INSERT INTO top_songs VALUES (" + str(x) + "," + top_songs[x][0] + "," + top_songs[x][1] + "," + top_songs[x][2] + "," + top_songs[x][3] + ");"
         db.execute(insert_statement)
 
     for x in range(1, len(top_artists) + 1):
-        insert_statement = "INSERT INTO top_artists VALUES (" + x + "," + top_artists[x][0] + "," + top_artists[x][1] + "," + top_artists[x][2] + ");"
+        insert_statement = "INSERT INTO top_artists VALUES (" + str(x) + "," + top_artists[x][0] + "," + top_artists[x][1] + "," + top_artists[x][2] + ");"
         db.execute(insert_statement)
 
     db.commit()
